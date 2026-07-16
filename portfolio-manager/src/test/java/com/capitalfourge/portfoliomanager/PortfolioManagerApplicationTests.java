e4bf598ecd1841738d11978e8e84ebc2ac36729d
LeeankFM <leeankf@gmail.com>
2026-07-16 14:32:52 +0000

    fix(test): disable Redis completely in test profile

--- portfolio-manager/src/test/java/com/capitalfourge/portfoliomanager/PortfolioManagerApplicationTests.java
+++ portfolio-manager/src/test/java/com/capitalfourge/portfoliomanager/PortfolioManagerApplicationTests.java
@@ -3,6 +3,10 @@ package com.capitalfourge.portfoliomanager;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.session.SessionAutoConfiguration;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.test.context.ActiveProfiles;
@@ -10,7 +14,13 @@ import org.springframework.test.context.ActiveProfiles;
 // Disable embedded Redis auto-start for unit tests
 @SpringBootTest(properties = {
     "spring.data.redis.enabled=false",
-    "spring.redis.enabled=false"
+    "spring.redis.enabled=false",
+    "spring.redis.url="
+})
+@EnableAutoConfiguration(exclude = {
+    RedisAutoConfiguration.class,
+    RedisRepositoriesAutoConfiguration.class,
+    SessionAutoConfiguration.class
 })
 @ActiveProfiles("test")
 class PortfolioManagerApplicationTests {

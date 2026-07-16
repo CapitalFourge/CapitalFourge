9a96bfbb01bac367b2b74141a611c8c13ff0aa25
LeeankFM <leeankf@gmail.com>
2026-07-16 14:56:22 +0000

    fix: use NEXT_PUBLIC_API_BASE_URL for GraphQL endpoint in Apollo Client

--- frontend/lib/apollo-client.ts
+++ frontend/lib/apollo-client.ts
@@ -2,37 +2,38 @@ import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import { setAuthCookie } from './auth-cookie';
 
-// Function to get the API URI - works on both client and server
-const getApiUri = () => {
-  // If we're on the client, use window.location
+// Get GraphQL endpoint from environment variable
+const getGraphQLUri = () => {
   if (typeof window !== 'undefined') {
-    return 'http://' + window.location.hostname + ':8080/graphql';
+    // Client-side: use the environment variable
+    return process.env.NEXT_PUBLIC_API_BASE_URL 
+      ? `${process.env.NEXT_PUBLIC_API_BASE_URL}/graphql`
+      : 'http://localhost:8080/graphql';
   }
-  // If we're on the server, default to localhost (or could be configured via env)
-  return 'http://localhost:8080/graphql';
+  // Server-side: also use environment variable
+  return process.env.NEXT_PUBLIC_API_BASE_URL
+    ? `${process.env.NEXT_PUBLIC_API_BASE_URL}/graphql`
+    : 'http://localhost:8080/graphql';
 };
 
 const httpLink = createHttpLink({
-  uri: getApiUri(),
+  uri: getGraphQLUri(),
 });
 
-console.log("[DEBUG] Apollo Client configured for:", getApiUri());
-
 const authLink = setContext((_, { headers }) => {
-    const token = typeof window !== 'undefined' ? localStorage.getItem('access_token') : null;
-    if (token) {
-      setAuthCookie(token);
+  const token = typeof window !== 'undefined' ? localStorage.getItem('access_token') : null;
+  if (token) {
+    setAuthCookie(token);
+  }
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
     }
-    console.log("[DEBUG] Auth token retrieved:", !!token);
-    return {
-        headers: {
-            ...headers,
-            authorization: token ? `Bearer ${token}` : "",
-        }
-    }
+  };
 });
 
 export const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
 });

31dfa34c686aef9c14b110d5431744fe97dd4b65
LeeankFM <leeankf@gmail.com>
2026-07-18 12:45:48 +0000

    fix: disable all Redis health checks + debug logging

--- portfolio-manager/src/main/java/com/capitalfourge/portfoliomanager/PortfolioManagerApplication.java
+++ portfolio-manager/src/main/java/com/capitalfourge/portfoliomanager/PortfolioManagerApplication.java
@@ -6,12 +6,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.redis.RedisReactiveHealthIndicatorAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 import com.capitalfourge.portfoliomanager.infrastructure.grpc.GrpcFinancialDataClient;
 
-@SpringBootApplication(exclude = {RedisReactiveHealthIndicatorAutoConfiguration.class})
+@SpringBootApplication
 public class PortfolioManagerApplication {
 
     public static void main(String[] args) {

d7743f1bad4ad9f66a4576fc126f4e38b7331098
LeeankFM <leeankf@gmail.com>
2026-07-18 02:32:47 +0000

    fix: revert exclude, use reactive.redis.enabled=false in config

--- portfolio-manager/src/main/java/com/capitalfourge/portfoliomanager/PortfolioManagerApplication.java
+++ portfolio-manager/src/main/java/com/capitalfourge/portfoliomanager/PortfolioManagerApplication.java
@@ -5,12 +5,11 @@ import java.util.List;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.redis.RedisReactiveHealthIndicatorAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 import com.capitalfourge.portfoliomanager.infrastructure.grpc.GrpcFinancialDataClient;
 
-@SpringBootApplication(exclude = {RedisReactiveHealthIndicatorAutoConfiguration.class})
+@SpringBootApplication
 public class PortfolioManagerApplication {
 
     public static void main(String[] args) {

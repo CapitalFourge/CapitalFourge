43629bd0933e630e3819a0914b73ab0c0cb0834d
LeeankFM <leeankf@gmail.com>
2026-08-05 04:07:08 +0000

    fix: CommandLineRunner fixes NULL version in DB at startup
    - Runs UPDATE users SET version=0 WHERE version IS NULL on app start
    - Definitive fix for 'Cannot invoke Long.longValue() because current is null'
    - @PostLoad was too late for Hibernate optimistic locking check

--- portfolio-manager/src/main/java/com/capitalfourge/portfoliomanager/PortfolioManagerApplication.java
+++ portfolio-manager/src/main/java/com/capitalfourge/portfoliomanager/PortfolioManagerApplication.java
@@ -1,12 +1,25 @@
 package com.capitalfourge.portfoliomanager;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class PortfolioManagerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PortfolioManagerApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner fixNullVersions(JdbcTemplate jdbcTemplate) {
+        return args -> {
+            int updated = jdbcTemplate.update("UPDATE users SET version = 0 WHERE version IS NULL");
+            if (updated > 0) {
+                System.out.println("Fixed " + updated + " users with NULL version");
+            }
+        };
     }
 }
